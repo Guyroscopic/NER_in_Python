34dd10a6fbfe7773651bd84cae915d273e93efd5
unknown <arafey183@gmail.com>
2021-10-15 15:17:15 +0500

    Generated char_embeddings.pkl file

--- generate_char_embeddings.py
+++ generate_char_embeddings.py
@@ -1,15 +1,19 @@
 import pickle
 import numpy as np
-
 np.random.seed(10)      #to reproduce results
 
-import os
-print(os.listdir('./'))
+embedding_dim   = 30
+limit           = np.sqrt(3 / embedding_dim)
 
-# with open('unique_chars.pkl', 'rb') as f:
-#     chars = pickle.load(f)
+with open('unique_chars.pkl', 'rb') as f:
+    chars = pickle.load(f)
 
-#for i in range(len(chars)):
 
+char_embeddings = dict()
+for c in chars:
+    char_embeddings[c] = np.random.uniform(-limit, limit, embedding_dim)
+
+with open('char_embeddings.pkl', 'wb') as f:
+    pickle.dump(char_embeddings, f) 
 
 
